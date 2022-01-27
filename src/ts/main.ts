201cb8a4982721d86d0568e40d7db4aadf23a138
堺 達彦 <tatsuhiko.sakai@north-s.co.jp>
2022-01-27 19:26:08 +0900

    add - 背景アニメーションの仮実装

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,11 +1,72 @@
-// import './style.scss'
-import sample from './sample';
+import _ from 'lodash';
+// import sample from './sample';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+window.addEventListener('DOMContentLoaded', () => {
+  const elemBody = document.querySelector('body')!;
+  const elemCircle = document.createElement('div');
+  const bgTag =  document.querySelector('.bg')!;
+  let windowWidth: number;
+  let windowHeight: number;
+  let bodyHeight: number;
+  // let targetScroll = 0;
+  let currentScroll = 0;
+  // let scrollOffset = 0;
+  const circleSize = 20;
 
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+  init();
+  circularMoving();
 
-sample();
+
+  // 初期実行処理
+  function init() {
+    // 背景の要素のDOM要素追加
+    elemCircle.classList.add('circle');
+    bgTag.insertAdjacentElement('beforeend', elemCircle);
+
+    setWindowSize();
+    window.addEventListener('resize', _.debounce(() => {
+      setWindowSize();
+    }, 300));
+  }
+
+  // windowサイズの保持
+  function setWindowSize() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    bodyHeight = elemBody.scrollHeight;
+
+    console.log(windowWidth, windowHeight);
+  }
+
+  // 円運動
+  function circularMoving(){
+    updateScroll();
+
+    const circularRadius = 500;
+    const rad = currentScroll / (bodyHeight - windowHeight) * Math.PI*2 *2;
+    const x = Math.cos(rad) * circularRadius - circleSize/2;
+    const y = Math.sin(rad) * circularRadius/3 - circleSize/2;
+
+    elemCircle.setAttribute('style', `transform: translate(${x}px, ${y}px);`);
+
+    requestAnimationFrame(circularMoving);
+    // console.log(currentScroll);
+  }
+
+  window.addEventListener('scroll', () => {
+    // console.log(document.documentElement.scrollTop);
+  });
+
+  // スクロール位置を取得、滑らかに補間
+  function updateScroll(){
+    const targetScroll = document.documentElement.scrollTop;
+    currentScroll = lerp(currentScroll, targetScroll, 0.1);
+    // scrollOffset = targetScroll - currentScroll;
+    // console.log(targetScroll, currentScroll, scrollOffset);
+  }
+
+  // リープ関数
+  function lerp(start: number, end: number, multiplier: number): number{
+    return (1 - multiplier) * start + multiplier * end;
+  }
+});
