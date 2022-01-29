390ffc512dc83fae665630b53b4fc0a96c782d90
futafusa <seahorse2000@gmail.com>
2022-01-29 19:52:54 +0900

    add - 全体的な仮実装終わり

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -9,9 +9,11 @@ window.addEventListener('DOMContentLoaded', () => {
   let windowHeight: number;
   let bodyHeight: number;
 
-  let circleSize = 20;
-  let circularRadius = 500;
+  let circleSize = 18;
+  let circularRadius = 160;
+  let circularHeightRatio = 6;
   let currentScroll = 0;
+  let scrollVector = 0
 
   const optionIO = {
     root: null,
@@ -45,16 +47,27 @@ window.addEventListener('DOMContentLoaded', () => {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
     bodyHeight = elemBody.scrollHeight;
+    
+    if(windowWidth <= 768) {
+      circularRadius = 180;
+      // circleSize = 4;
+    } else if(windowWidth <= 1400) {
+      circularRadius = 500;
+      // circleSize = 20;
+    } else {
+      circularRadius = 700;
+    }
 
-    // 軌跡サイズの調整
+    // 惑星・軌跡サイズの調整
+    // elemCircle.setAttribute('style', `width: ${circleSize}px; height: ${circleSize}px`);
     elemTrajectory.setAttribute(
       'style',
       `
         width: ${circularRadius*2}px;
-        height: ${circularRadius/2}px;
-        margin-top: -${circularRadius/4}px;
+        height: ${circularRadius*2/circularHeightRatio}px;
+        margin-top: -${circularRadius/circularHeightRatio}px;
         margin-left: -${circularRadius}px;
-        border-radius: ${circularRadius*2}px / ${circularRadius/2}px;
+        border-radius: ${circularRadius*2}px / ${circularRadius*2/circularHeightRatio}px;
       `
     );
     console.log(windowWidth, windowHeight);
@@ -68,9 +81,10 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const rad = currentScroll / (bodyHeight - windowHeight) * Math.PI*2 *2;
     const x = Math.cos(rad) * circularRadius - circleSize/2;
-    const y = Math.sin(rad) * circularRadius/4 - circleSize/2;
+    const y = Math.sin(rad) * circularRadius/circularHeightRatio - circleSize/2;
 
     elemCircle.setAttribute('style', `transform: translate(${x}px, ${y}px);`);
+    elemBody.style.backgroundPositionY = `${-scrollVector/40}px`;
     requestAnimationFrame(circularMoving);
   }
 
@@ -78,6 +92,8 @@ window.addEventListener('DOMContentLoaded', () => {
   function updateScroll(){
     const targetScroll = document.documentElement.scrollTop;
     currentScroll = lerp(currentScroll, targetScroll, 0.1);
+    scrollVector = lerp(currentScroll, targetScroll, 0.01);
+    // console.log(scrollVector);
   }
 
   // リープ関数
